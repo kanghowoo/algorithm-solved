05a1d9b9059101152818c7cfb13392b9966aa27d
Lee KangHo <tiger452003@gmail.com>
2023-12-24 23:59:17 +0900

    [Silver III] Title: 안테나, Time: 1344 ms, Memory: 179960 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int[] nums = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(nums);
+
+        System.out.println(nums[(N - 1)/2]);
+
+    }
+}
