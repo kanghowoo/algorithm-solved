434600a68b216f5244f788113a57d3513929194f
Lee KangHo <tiger452003@gmail.com>
2024-02-03 23:03:56 +0900

    [Silver II] Title: 가장 큰 감소 부분 수열, Time: 280 ms, Memory: 20448 KB -BaekjoonHub

---
+++ 백준/Silver/17216. 가장 큰 감소 부분 수열/가장 큰 감소 부분 수열.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int[] dp = new int[N];
+
+        dp[0] = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+        }
+
+        Arrays.sort(dp);
+        System.out.println(dp[N - 1]);
+    }
+}
