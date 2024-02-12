510b88488a8b3ab48325acf42ebbaa7c921a1296
Lee KangHo <tiger452003@gmail.com>
2024-02-12 23:33:50 +0900

    [Gold IV] Title: 줄세우기, Time: 220 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        Arrays.sort(dp);
+
+        int len = dp[N - 1];
+
+        System.out.println(N - len);
+        
+    }
+}
