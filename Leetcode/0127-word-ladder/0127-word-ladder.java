279b04d3f6015c5b28bc38c9d052ccd2c859a410
Lee KangHo <tiger452003@gmail.com>
2025-02-02 19:42:31 +0900

    Time: 92 ms (49.48%), Space: 46.2 MB (75.2%) - LeetHub

--- Leetcode/0127-word-ladder/0127-word-ladder.java
+++ Leetcode/0127-word-ladder/0127-word-ladder.java
@@ -2,39 +2,43 @@ import java.util.*;
 
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> wordSet = new HashSet<>(wordList); // O(N)
-        if (!wordSet.contains(endWord)) return 0; // O(1)
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) return 0;
 
         Queue<String> queue = new LinkedList<>();
-        queue.offer(beginWord); // O(1)
-        int level = 1;
 
-        while (!queue.isEmpty()) { // 최대 O(N) 반복
-            int size = queue.size(); // O(1)
-            
-            for (int i = 0; i < size; i++) { // 최대 O(N) 반복
-                String word = queue.poll(); // O(1)
+        queue.offer(beginWord);
+        wordSet.remove(beginWord);
+        int result = 1;
 
-                for (int j = 0; j < word.length(); j++) { // O(L) (L = 단어 길이)
-                    char[] wordArray = word.toCharArray(); // O(L)
-                    
-                    for (char c = 'a'; c <= 'z'; c++) { // O(26)
-                        if (wordArray[j] == c) continue; // O(1)
+        while (!queue.isEmpty()) {
+            int qSize = queue.size();
+
+            for (int i = 0; i < qSize; i++) {
+                String word = queue.poll();
+                
+                for (int j = 0; j < word.length(); j++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        char[] wordArray = word.toCharArray();                        
                         
+                        if (wordArray[j] == c) continue;
+
                         wordArray[j] = c;
-                        String newWord = new String(wordArray); // O(L)
+                        String newWord = new String(wordArray);
 
-                        if (newWord.equals(endWord)) return level + 1; // O(L)
-                        
-                        if (wordSet.contains(newWord)) { // O(1)
-                            queue.offer(newWord); // O(1)
-                            wordSet.remove(newWord); // O(1)
+                        if (newWord.equals(endWord)) return result + 1;
+
+                        if (wordSet.contains(newWord)) {
+                            queue.offer(newWord);
+                            wordSet.remove(newWord);
                         }
                     }
                 }
             }
-            level++; 
+
+            result++;
         }
+
         return 0;
     }
 }
