276dbabe7feb74cc120eaa87018c5a2381a16172
Lee KangHo <tiger452003@gmail.com>
2025-04-19 04:48:01 +0900

    Time: 5 ms (33.04%), Space: 52.2 MB (21.57%) - LeetHub

--- Leetcode/0200-number-of-islands/0200-number-of-islands.java
+++ Leetcode/0200-number-of-islands/0200-number-of-islands.java
@@ -1,15 +1,3 @@
-class Tile {
-    int x;
-    int y;
-    int c;
-
-    public Tile(int x, int y, int c) {
-        this.x = x;
-        this.y = y;
-        this.c = c;
-    }
-}
-
 class Solution {
     static int m;
     static int n;
@@ -17,13 +5,14 @@ class Solution {
     public int numIslands(char[][] grid) {
         m = grid.length;
         n = grid[0].length;
+
         int result = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
                     result++;
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                 }
             }
         }
@@ -31,25 +20,27 @@ class Solution {
         return result;
     }
 
-    private void dfs(char[][] grid, int x, int y) {
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+    public void bfs(char[][] grid, int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
 
-        Queue<Tile> tiles = new ArrayDeque();        
-        tiles.offer(new Tile(x, y, '1'));
+        grid[x][y] = '0';
 
-        while (!tiles.isEmpty()) {
-            Tile tile = tiles.poll();
+        int[][] directions = {{-1,0}, {1,0}, {0, -1}, {0, 1}}; //상하좌우
 
-            for (int i = 0; i < 4; i++) {
-                int nx = tile.x + dx[i];
-                int ny = tile.y + dy[i];
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            for (int[] dir : directions) {
+                int nx = current[0] + dir[0];
+                int ny = current[1] + dir[1];
 
                 if (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == '1') {
-                    tiles.offer(new Tile(nx, ny, '1'));
+                    queue.offer(new int[]{nx, ny});
                     grid[nx][ny] = '0';
                 }
             }
         }
+
     }
 }
